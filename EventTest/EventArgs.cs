ca907d8b22b50f7b2bea556ffcefec0fffa150ef
amantaw <amantaw3@gmail.com>
2025-01-14 21:53:39 -0500

    Add project files.

---
+++ EventTest/EventArgs.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTest
+{
+    public class TemperatureChangedEventArgs : EventArgs
+    {
+        public int OldTemperature { get; }
+        public int NewTemperature { get; }
+
+        public TemperatureChangedEventArgs(int oldTemperature, int newTemperature)
+        {
+            OldTemperature = oldTemperature;
+            NewTemperature = newTemperature;
+        }
+        public delegate void TemperatureChangedEventHandler(object sender, TemperatureChangedEventArgs e);
+
+        public class TemperatureSensor
+        {
+            private int _currentTemperature;
+
+            public event TemperatureChangedEventHandler TemperatureChanged;
+
+            public int CurrentTemperature
+            {
+                get { return _currentTemperature; }
+                set
+                {
+                    if (_currentTemperature != value)
+                    {
+                        int oldTemperature = _currentTemperature;
+                        _currentTemperature = value;
+                        OnTemperatureChanged(oldTemperature, _currentTemperature);
+                    }
+                }
+            }
+
+            protected virtual void OnTemperatureChanged(int oldTemperature, int newTemperature)
+            {
+                // Check if there are any subscribers and invoke the event
+                TemperatureChanged?.Invoke(this, new TemperatureChangedEventArgs(oldTemperature, newTemperature));
+            }
+        }
+        public class TemperatureDisplay
+        {
+            public void OnTemperatureChanged(object sender, TemperatureChangedEventArgs e)
+            {
+                Console.WriteLine($"Temperature changed from {e.OldTemperature}°C to {e.NewTemperature}°C.");
+            }
+        }
+
+
+    }
+}
---
+++ EventTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using static EventTest.TemperatureChangedEventArgs;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Create instances of the classes
+        TemperatureSensor sensor = new TemperatureSensor();
+        TemperatureDisplay display = new TemperatureDisplay();
+
+        // Subscribe to the event
+        sensor.TemperatureChanged += display.OnTemperatureChanged;
+
+        // Change the temperature, triggering the event
+        sensor.CurrentTemperature = 25; // Event triggered here
+        sensor.CurrentTemperature = 30; // Event triggered here
+    }
+}
+
